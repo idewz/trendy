418eb28ec48c9dabe260782ef022e4e301c24836
Siwadon Saosoong <siwadon.s@gmail.com>
2015-07-16 17:18:59 +0700

    Retrieve share counts from Facebook Graph API

--- counter.js
+++ counter.js
@@ -1,7 +1,10 @@
-var redis = require('redis');
-var Q     = require('q');
+var _       = require('lodash');
+var redis   = require('redis');
+var request = require('request');
+var Q       = require('q');
 
 function Counter() {
+  this.time_range = 86400;
 }
 
 Counter.prototype.fetch = function() {
@@ -9,8 +12,8 @@ Counter.prototype.fetch = function() {
   var client   = redis.createClient();
 
   var now  = Math.floor(new Date() / 1000);
-  var from = now - (60 * 60);
-  console.log('fetch urls_log starting from', from, ' to ', now);
+  var from = now - this.time_range;
+  console.log('fetch urls_log starting from', from, 'to', now);
 
   client.zrangebyscore('urls_log', from, now, function(err, urls) {
     if (err) {
@@ -21,6 +24,45 @@ Counter.prototype.fetch = function() {
   });
 
   return deferred.promise;
+};
+
+Counter.prototype.fetch_facebook = function(urls) {
+  var limit = 50;
+  var endpoint = 'https://graph.facebook.com/?access_token=' + process.env.FACEBOOK_TOKEN + '&ids=';
+
+  _.forEach(_.chunk(urls, limit), function(list) {
+    var url = [
+      endpoint,
+      list.join()
+    ];
+    var options = {
+      url: url.join('')
+    };
+
+    request.get(options, function(err, res, body) {
+      console.log('fetched ' + url.join(''));
+
+      if (err) {
+        console.error(err);
+      } else if (res.statusCode !== 200) {
+        console.error(body);
+      } else {
+        var objects   = JSON.parse(body);
+        var client    = redis.createClient();
+        var timestamp = Math.floor(new Date() / 1000);
+
+        _.forEach(objects, function(obj) {
+          var url = obj.og_object.url;
+          var share_count = obj.share.share_count;
+          client.zadd('fetch_log', 'NX', timestamp, url, redis.print);
+          // client.zadd('rank', share_count, url, redis.print);
+          client.set('counts:' + url, share_count, redis.print);
+        });
+      }
+    });
+  });
+
+  return true;
 };
 
 module.exports = Counter;
