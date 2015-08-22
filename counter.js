1517890a3708b68e74b95956045c18653c401cf4
Siwadon Saosoong <siwadon.s@gmail.com>
2015-08-22 21:55:25 +0700

    Add timestamp to urls list to make it possible to get shares/time difference

--- counter.js
+++ counter.js
@@ -9,6 +9,18 @@ function Counter() {
   this.time_range = 86400;
 }
 
+set_shares_history = function(url, hours_diff, share_count) {
+  db.redis.hget(url, 'counts', function(err, reply) {
+    var shares_diff;
+    if (err) {
+      shares_diff = 0;
+    } else if (reply) {
+      shares_diff = share_count - reply;
+    }
+    db.redis.hset('history:' + url, hours_diff, shares_diff, db.redis.print);
+  });
+};
+
 Counter.prototype.fetch = function() {
   var deferred = Q.defer();
   var client   = db.redis;
@@ -17,26 +29,29 @@ Counter.prototype.fetch = function() {
   var from = now - this.time_range;
   console.log('fetch urls_log starting from', from, 'to', now);
 
-  client.zrangebyscore('urls_log', from, now, function(err, urls) {
+  client.zrangebyscore('urls_log', from, now, 'WITHSCORES', function(err, urls) {
     if (err) {
       deferred.reject(err);
     } else {
-      deferred.resolve(urls);
+      var urls_and_times = _.partition(urls, function(str) { return Number.isNaN(parseInt(str, 10)); });
+      deferred.resolve(urls_and_times);
     }
   });
 
   return deferred.promise;
 };
 
-Counter.prototype.fetch_facebook = function(urls) {
+Counter.prototype.fetch_facebook = function(urls_and_times) {
   var limit = 50;
   var endpoint = 'https://graph.facebook.com/?access_token=' + process.env.FACEBOOK_TOKEN + '&ids=';
+  var urls  = urls_and_times[0];
+  var times = urls_and_times[1];
 
-  _.forEach(_.chunk(urls, limit), function(list) {
+  _.forEach(_.chunk(urls, limit), function(item) {
     var counting = function() {
       var url = [
         endpoint,
-        list.join()
+        item.join(',')
       ];
       var options = {
         url: url.join('')
@@ -52,7 +67,7 @@ Counter.prototype.fetch_facebook = function(urls) {
         } else {
           var objects   = JSON.parse(body);
           var client    = db.redis;
-          var timestamp = moment().unix();
+          var timestamp = moment();
           var og_url;
           var share_count;
           var title;
@@ -66,12 +81,25 @@ Counter.prototype.fetch_facebook = function(urls) {
               og_url = obj.id;
               share_count = obj.share.share_count;
               title = obj.id;
+            } else {
+              return;
             }
 
-            client.zadd('fetch_log', 'NX', timestamp, og_url, db.print);
+            client.zadd('fetch_log', 'NX', timestamp.unix(), og_url, db.print);
             // client.zadd('rank', share_count, url, db.print);
             client.hset(og_url, 'title', title, db.print);
             client.hset(og_url, 'counts', share_count, db.print);
+
+            // set url share_count history by hours diff
+            var urls_with_time = _.object(urls, times);
+            if (_.isUndefined(urls_with_time[og_url])) {
+              console.log('can\'t find logged time for ' + og_url);
+              return;
+            }
+            var logged_time = moment(urls_with_time[og_url] * 1000);
+            var time_diff   = timestamp.diff(logged_time, 'hours');
+
+            set_shares_history(og_url, time_diff, share_count);
           });
         }
       });
@@ -82,19 +110,19 @@ Counter.prototype.fetch_facebook = function(urls) {
   return true;
 };
 
-Counter.prototype.rank = function(urls) {
+Counter.prototype.rank = function(urls_and_times) {
   var deferred = Q.defer();
   var client   = db.redis;
   var list     = [];
 
-  _.forEach(urls, function(url) {
+  _.forEach(urls_and_times[0], function(url) {
     client.hmget(url, 'title', 'counts', function(err, replies) {
       list.push({
         url: url,
         title: replies[0],
         share_count: parseInt(replies[1]) ||  0
       });
-      if (list.length == urls.length) {
+      if (list.length == urls_and_times[0].length) {
         deferred.resolve(_.sortByOrder(list, 'share_count', 'desc'));
       }
     });
